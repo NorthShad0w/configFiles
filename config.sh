ed9fcbe91b2086d466e44977ba58a49248e94922
northshad0w <north@shad0w.com>
2021-08-18 09:24:04 -0400

    1

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo echo 'ServerAliveInterval 50' >> /etc/ssh/ssh_config
-wget https://raw.githubusercontent.com/NorthShad0w/configFiles/main/qterminal.ini -O ~/.config/qterminal.org/qterminal.ini
-exo-open --launch TerminalEmulator
-exit
+#wget https://raw.githubusercontent.com/NorthShad0w/configFiles/main/qterminal.ini -O ~/.config/qterminal.org/qterminal.ini
+#exo-open --launch TerminalEmulator
+#exit
