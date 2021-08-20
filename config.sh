177887731eb3a454bf7b0acbe9145969db6696e1
northshad0w <north@shad0w.com>
2021-08-20 12:16:37 -0400

    update

--- config.sh
+++ config.sh
@@ -12,10 +12,18 @@ cp my_zshrc ~/.zshrc
 sudo apt install i3-wm i3status suckless-tools
 cp i3_congfig ~/.config/i3/config
 
+#install requirements
+sudo apt install feh cargo
+
+#set wallpaper
+cp dark.jpg ~/.config/dark.jpg
+cp .fehbg ~/.fehbg
+
+
 #install alacritty
 proxychains git clone https://github.com/alacritty/alacritty.git
 cd alacritty
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 proxychains cargo build --release && sudo cp target/release/alacritty /usr/local/bin
 
 #wget https://raw.githubusercontent.com/NorthShad0w/configFiles/main/qterminal.ini -O ~/.config/qterminal.org/qterminal.ini
