3baab8ee3b776fda859f11e8c993dc710c672f80
NorthShad0w <72195616+NorthShad0w@users.noreply.github.com>
2022-04-24 11:12:13 +0800

    Update keymaps.lua

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,22 @@
--- to do
--- add some keymaps
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--Remap NvimTreeToggle
+keymap("n", "<leader>d", ":NvimTreeToggle<CR>", opts)
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
